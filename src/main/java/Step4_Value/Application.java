e8333ca93e1926f2e6d13e5c02063019f1c154a7
nail111 <g.alpha@mail.ru>
2020-08-08 15:25:09 +0400

    08.08.2020

---
+++ src/main/java/Step4_Value/Application.java
@@ -0,0 +1,32 @@
+package Step4_Value;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
+
+
+@RestController
+class SayHello {
+
+    @Value("${name}")
+    private String name;
+
+    @Value("${year}")
+    private int year;
+
+    @GetMapping("/Samsung")
+    public String getInfo() {
+        return name + " " + year;
+    }
+}
